6d6dd713be8481f0bffc58e70510ad5c3cd8b929
dacdat190201 <94001385+dacdat190201@users.noreply.github.com>
2024-06-15 01:47:00 +0700

    build header img

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import logo from "../assets/images/logo/logo.png";
 const TheHeader = () => {
   return (
-    <div className="home-header-container">
-      <div className="home-header-container-left">
-        <div>LOGO</div>
-        <div>NAME</div>
+    <div className="home-header-sidebar">
+      <div className="home-header-sidebar-left">
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="home-header-sidebar-title">Hounter</div>
       </div>
-      <div className="home-header-container-right">
+      <div className="home-header-sidebar-right">
         <div>LOGO</div>
         <div>NAME</div>
       </div>
--- src/container/TheLayout.js
+++ src/container/TheLayout.js
@@ -1,9 +1,17 @@
 import React from "react";
 import TheHeader from "../components/TheHeader";
+import headerShadowsbox from "../assets/images/backgrounds/header-shadowsbox.png";
+import headerHome from "../assets/images/backgrounds/header-home.png";
 
 const TheLayout = () => {
   return (
-    <div className="c-wapper-home">
+    <div className="c-wapper-home-header">
+      <div className="header-background-left">
+        <img src={headerShadowsbox} alt="" />
+      </div>
+      <div className="header-background-right">
+        <img src={headerHome} alt="" />
+      </div>
       <TheHeader />
     </div>
   );
