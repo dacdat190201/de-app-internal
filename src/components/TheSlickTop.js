8310bf059d0c9f883db79de3d5ae995b47f93c1f
dacdat190201 <94001385+dacdat190201@users.noreply.github.com>
2024-06-17 22:43:58 +0700

    slide to show

--- src/components/TheSlickTop.js
+++ src/components/TheSlickTop.js
@@ -1,7 +1,58 @@
+import { Avatar } from "antd";
 import React from "react";
 
 const TheSlickTop = () => {
-  return <div className="slick-top">TheSlickTop</div>;
+  return (
+    <div className="custom-slick-slider">
+      <div className="custom-slick-slider-item">
+        <div className="custom-slick-slider-list-avatar">
+          <Avatar.Group>
+            <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />
+            <a href="https://ant.design">
+              <Avatar
+                style={{
+                  backgroundColor: "#f56a00",
+                }}
+              >
+                K
+              </Avatar>
+            </a>
+            <Avatar
+              style={{
+                backgroundColor: "#87d068",
+              }}
+            />
+          </Avatar.Group>
+        </div>
+        <div className="custom-slick-slider-item-title">
+          1K+ Prople
+          <span>Suscessfully Getting Home</span>
+        </div>
+      </div>
+      <div className="custom-slick-slider-item">
+        <div className="custom-slick-slider-list-avatar">
+          <Avatar.Group>
+            <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />
+          </Avatar.Group>
+        </div>
+        <div className="custom-slick-slider-item-title">
+          56 Houses
+          <span>Sold Monthly</span>
+        </div>
+      </div>
+      <div className="custom-slick-slider-item">
+        <div className="custom-slick-slider-list-avatar">
+          <Avatar.Group>
+            <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />
+          </Avatar.Group>
+        </div>
+        <div className="custom-slick-slider-item-title">
+          4K+
+          <span>People Looking for New Homes</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TheSlickTop;
