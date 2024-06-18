a5743b8a28d671b87f8bc5992c858a495909583b
dat.nguyend <dat.nguyend@s3corp.com.vn>
2024-06-18 14:41:53 +0700

    mbuild footer, slider

--- src/components/TheSlickTop.js
+++ src/components/TheSlickTop.js
@@ -1,18 +1,22 @@
 import { Avatar } from "antd";
 import React from "react";
-
+import home1 from "../assets/slick/Rectangle 23.png";
 const TheSlickTop = () => {
   return (
     <div className="custom-slick-slider">
       <div className="custom-slick-slider-item">
         <div className="custom-slick-slider-list-avatar">
           <Avatar.Group>
-            <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />
+            <Avatar
+              src="https://api.dicebear.com/7.x/miniavs/svg?seed=1"
+              size="large"
+            />
             <a href="https://ant.design">
               <Avatar
                 style={{
                   backgroundColor: "#f56a00",
                 }}
+                size="large"
               >
                 K
               </Avatar>
@@ -21,6 +25,7 @@ const TheSlickTop = () => {
               style={{
                 backgroundColor: "#87d068",
               }}
+              size="large"
             />
           </Avatar.Group>
         </div>
@@ -32,12 +37,23 @@ const TheSlickTop = () => {
       <div className="custom-slick-slider-item">
         <div className="custom-slick-slider-list-avatar">
           <Avatar.Group>
-            <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />
+            <Avatar src={home1} size="large" />
           </Avatar.Group>
         </div>
         <div className="custom-slick-slider-item-title">
           56 Houses
           <span>Sold Monthly</span>
+        </div>
+      </div>
+      <div className="custom-slick-slider-item">
+        <div className="custom-slick-slider-list-avatar">
+          <Avatar.Group>
+            <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />
+          </Avatar.Group>
+        </div>
+        <div className="custom-slick-slider-item-title">
+          4K+
+          <span>People Looking for New Homes</span>
         </div>
       </div>
       <div className="custom-slick-slider-item">
